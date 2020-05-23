e6753ba5c4d1331fa610f543d27dcfd3cc4201b5
Puspa Gurung <gpuspamail@gmail.com>
2020-05-23 23:41:14 +0400

    configure scss, add most common mixins, variables, utilities classes, add google font link

---
+++ src/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './scss/main.scss';
+import { BrowserRouter } from 'react-router-dom';
+import Router from './Router';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
+  );
+};
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
